ff83eba3facd81de486ceca23930314328c77ea4
Sanuja Rubasinghe <sanurubasinghe1234@gmai.com>
2024-06-17 13:00:53 +0530

    Correct mistakes

--- server/index.js
+++ server/index.js
@@ -10,7 +10,13 @@ const io = Server(httpServer, {
 })
 
 io.on('connection', socket => {
+    console.log(`User ${socket.id} connected.`)
     socket.on('message', data => {
-        socket.emit('message', `${data}`)
+        console.log(data)
+        io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
     })
+})
+
+httpServer.listen(3500, () => {
+    console.log('listening on port 3500')
 })
